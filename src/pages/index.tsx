caa5033df5a1ed3f0195d0c02983f2674d98c7dd
galeno <gui.galenocover@gmail.com>
2022-01-18 16:14:42 -0300

    Structure of Table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 export default function Home() {
+  const clients = [
+    new Client("Galeno", 19, "1"),
+    new Client("Lua", 18, "2"),
+    new Client("Gusta", 18, "3"),
+    new Client("Isa", 13, "4"),
+    new Client("Apollo", 17, "5"),
+  ];
+
   return (
     <div
       className={`
-      flex h-screen
-      justify-center
-      items-center
-      bg-gradient-to-r
-      from-purple-500
-      to-blue-300
-      text-white`}
+      flex h-screen justify-center items-center bg-gradient-to-r
+      from-purple-500 to-blue-300 text-white`}
     >
-      <Layout title='Simple | Sig-in'>
-        <span>Content</span>
+      <Layout title="Simple | Sig-in">
+        <Table clients={clients}></Table>
       </Layout>
     </div>
   );
