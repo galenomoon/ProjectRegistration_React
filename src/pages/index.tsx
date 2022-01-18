7cc2a7494bac3b47a2bfec192e2a45cf9541c334
galeno <gui.galenocover@gmail.com>
2022-01-18 15:29:13 -0300

    Developing Components | Layout and Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
-import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <div
-      className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-300`}
+      className={`
+      flex h-screen
+      justify-center
+      items-center
+      bg-gradient-to-r
+      from-purple-500
+      to-blue-300
+      text-white`}
     >
-      <span className="text-4x1">Hello</span>
+      <Layout title='Simple | Sig-in'>
+        <span>Content</span>
+      </Layout>
     </div>
   );
 }
