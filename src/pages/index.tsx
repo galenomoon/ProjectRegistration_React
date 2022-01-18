85042d26a9de68bc749f96fad228d6f449d92675
galeno <gui.galenocover@gmail.com>
2022-01-18 15:02:22 -0300

    Start Point

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div
+      className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-300`}
+    >
+      <span className="text-4x1">Hello</span>
+    </div>
+  );
+}
